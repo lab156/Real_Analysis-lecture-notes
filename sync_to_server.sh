c2053d75ea9ca842f72a6d5567848510d371c119
Luis <Luis>
2015-03-15 19:19:38 -0400

    implement the sync to server option for remote work

---
+++ sync_to_server.sh
@@ -0,0 +1,7 @@
+source ./course_info
+if [ -n "$SYNCTOSERVER" ]; then
+    echo "......syncing.......";
+    cp this_paper.pdf /srv/http/this_paper.pdf ;
+else
+    echo "...NO SYNC WILL BE DONE...";
+fi
